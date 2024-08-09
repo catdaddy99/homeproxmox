f8d51a91b7f290928858f1a672fd2e7048da3eb6
Phillip Knowles <55642734+catdaddy99@users.noreply.github.com>
2024-08-09 19:53:29 -0400

    2

--- full-clone.tf
+++ full-clone.tf
@@ -2,13 +2,13 @@
 # ---
 # Create a new VM from a clone
 
-resource "proxmox_vm_qemu" "your-vm" {
+resource "proxmox_vm_qemu" "srv_demo_1" {
     
     # VM General Settings
-    target_node = "your-proxmox-node"
-    vmid = "100"
-    name = "vm-name"
-    desc = "Description"
+    target_node = "proxmox1"
+    vmid = "107"
+    name = "srv_demo_1"
+    desc = "TF deploy testing"
 
     # VM Advanced General Settings
     onboot = true 
@@ -20,18 +20,25 @@ resource "proxmox_vm_qemu" "your-vm" {
     agent = 1
     
     # VM CPU Settings
-    cores = 1
+    cores = 2
     sockets = 1
     cpu = "host"    
     
     # VM Memory Settings
-    memory = 1024
+    memory = 4096
 
     # VM Network Settings
     network {
         bridge = "vmbr0"
         model  = "virtio"
     }
+
+    # VM Disk Settings
+    disk {
+        storage = "local-lvm"
+        type  = "virtio"
+        size = "75G"
+    }    
 
     # VM Cloud-Init Settings
     os_type = "cloud-init"
