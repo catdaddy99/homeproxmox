c12c079bd7599736598ae17038472f1ed602d817
Phillip Knowles <55642734+catdaddy99@users.noreply.github.com>
2024-08-09 20:35:25 -0400

    11

--- full-clone.tf
+++ full-clone.tf
@@ -34,14 +34,14 @@ resource "proxmox_vm_qemu" "srv_demo_1" {
     }
 
     # VM Disk Settings
-    #disk {
-    #    storage = "local-lvm"
-    #    type  = "virtio"
-    #    size = "75G"
-    #}    
+    disk {
+        storage = "local-lvm"
+        type  = "virtio"
+        size = "75G"
+    }    
 
     # VM Cloud-Init Settings
-    os_type = "cloud-init"
+    #os_type = "cloud-init"
 
     # (Optional) IP Address and Gateway
     # ipconfig0 = "ip=0.0.0.0/0,gw=0.0.0.0"
