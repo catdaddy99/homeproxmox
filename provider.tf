758cb756035fdf2d7deb0f606c97c9732ce5d4a9
Phillip Knowles <55642734+catdaddy99@users.noreply.github.com>
2024-08-09 23:17:53 -0400

    update telmate proxmox version

--- provider.tf
+++ provider.tf
@@ -6,10 +6,16 @@ terraform {
 
     required_version = ">= 0.13.0"
 
+#    required_providers {
+#        proxmox = {
+#            source = "telmate/proxmox"
+#            version = ">= 2.9.14"
+#        }
+#    }
     required_providers {
         proxmox = {
-            source = "telmate/proxmox"
-            version = ">= 2.9.14"
+            source = "Telmate/proxmox"
+            version = "3.0.1-rc3"
         }
     }
 }
