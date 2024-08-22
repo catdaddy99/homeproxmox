d5a0fbba9b676d39e999ae92d80fdc01ad94be09
Phillip Knowles <55642734+catdaddy99@users.noreply.github.com>
2024-08-22 16:59:44 -0400

    Update full-clone.tf

--- full-clone.tf
+++ full-clone.tf
@@ -1,7 +1,7 @@
 
 resource "proxmox_lxc" "basic" {
   target_node  = "proxmox1"
-  hostname     = "lxc-basic"
+  hostname     = "pihole-ns1"
   ostemplate   = "local:vztmpl/ubuntu-23.04-standard_23.04-1_amd64.tar.zst"
   password     = "B00TY"
   unprivileged = true
@@ -19,7 +19,7 @@ resource "proxmox_lxc" "basic" {
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    ip     = "dhcp"
+    ip     = "10.77.2.200"
     tag = 2
   }
 }
